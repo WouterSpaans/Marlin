e0c588ef137bcbe04c7bde0d36aa3b28132bb9be
Wouter Spaans <wouterspaans@hotmail.com>
2019-11-30 23:15:51 +0100

    Fixed build error on incorrect NOZZLE_TO_PROBE_OFFSET

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -925,7 +925,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41.54, -16,.0, -1.85 }
+#define NOZZLE_TO_PROBE_OFFSET { -41.54, -15.10, -1.85 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
